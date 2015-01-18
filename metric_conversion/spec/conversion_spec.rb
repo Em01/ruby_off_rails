09d02d22944f9282543cee6fff6c836d6fa9a74c
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 18:20:14 +0000

    using expect instead of should

--- metric_conversion/spec/conversion_spec.rb
+++ metric_conversion/spec/conversion_spec.rb
@@ -5,10 +5,10 @@ describe Furlong do
 	let(:calculator) { Furlong.new }
 
 	it "converts 1 mile to 1.60934 km" do 
-		calculator.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
+		expect(calculator.miles_to_kilometers(1)).to be_within(0.0001).of(1.60934)
 	end
 
 	it 'converts a marathon: 26.219 miles to 42.194988 km' do 
-		calculator.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
+		expect(calculator.miles_to_kilometers(26.219)).to be_within(0.001).of(42.194988)
 	end
 end
