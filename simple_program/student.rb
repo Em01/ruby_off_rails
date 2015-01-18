29518f9df7e0e09a9acc09455a016df1e999ce04
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 14:14:06 +0000

    initial commit

---
+++ simple_program/student.rb
@@ -0,0 +1,41 @@
+class Student
+
+	attr_reader :first_name, :last_name, :grade
+	def initialize(first_name, last_name, grade)
+		@first_name = first_name
+		@last_name = last_name
+		@grade = grade
+	end
+
+	def senior?
+		grade == 12
+	end
+
+	def junior?
+		grade < 12
+	end
+
+	def to_s
+		"#{last_name}, #{first_name}"
+	end
+end
+
+def seniors(students)
+	students.select { |student| student.senior? }
+end
+
+def juniors(students)
+	students.select { |student| student.junior? }
+end
+fred = Student.new("Fred", "James", 12)
+emma = Student.new("Emma", "Williams", 12)
+millie = Student.new("Millie", "Wilson", 11)
+gong = Student.new("Gong", "Ash", 9)
+jack = Student.new("Jack", "Pipe", 7)
+all_students = [fred, emma, millie, gong, jack]
+
+juniors(all_students).each do |student|
+	puts student
+	end
+ 
+
