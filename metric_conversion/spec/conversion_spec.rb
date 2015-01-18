c1265a4a56b9d0e03d4f63393b310eb015bcab20
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 18:14:28 +0000

    refactoring

--- metric_conversion/spec/conversion_spec.rb
+++ metric_conversion/spec/conversion_spec.rb
@@ -2,15 +2,13 @@ require 'conversion'
 
 describe Furlong do 
 
+	let(:calculator) { Furlong.new }
+
 	it "converts 1 mile to 1.60934 km" do 
-		calc = Furlong.new
-		km = calc.miles_to_kilometers(1)
-		expect(km).to be_within(0.0001).of(1.60934)
+		calculator.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
 	end
 
 	it 'converts a marathon: 26.219 miles to 42.194988 km' do 
-		calc = Furlong.new
-		km = calc.miles_to_kilometers(26.219)
-		expect(km).to be_within(0.001).of(42.194988)
+		calculator.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
 	end
 end
