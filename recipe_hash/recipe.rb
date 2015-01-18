b8d7beaf8737f720d5f8d56799d1fce0ecdce920
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 15:26:14 +0000

    further work on struct

--- recipe_hash/recipe.rb
+++ recipe_hash/recipe.rb
@@ -11,11 +11,11 @@ recipe.ingredients = ingredients
 recipe.method = ["Peel / Slice Avocados", "Chop jalapenos into small dice"]
 
 puts "ingredients"
-recipe[:ingredients].each do |key, value|
+recipe.ingredients.each do |key, value|
 	puts "* #{key}: #{value}"
 end
 
 puts "\nMethod"
-recipe[:method].each_with_index do |step, index|
+recipe.method.each_with_index do |step, index|
 	puts "#{index+1}, #{step}"
 end
