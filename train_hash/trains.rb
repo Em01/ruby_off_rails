49cde61d4400349b4d95dcd092db475d4696a6f8
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 16:12:26 +0000

    station struct

--- train_hash/trains.rb
+++ train_hash/trains.rb
@@ -4,18 +4,19 @@ train[:engines] = 2
 train[:cars] = 1
 train[:caboose] = 1
 
-station = {}
-station[:train] = train
-station[:instructions] = ["Get on train", "Drive the train"]
+Station = Struct.new(:train, :instructions)
+station = Station.new
+station.train = train
+station.instructions = ["Get on train", "Drive the train"]
 
 puts "train"
 station[:train].each do |key, value|
-	puts "#{key}: #{value}"
+	puts "* #{key}: #{value}"
 end
 
 puts "\nInstructions"
-station[:instructions].each do |step|
-	puts step
+station[:instructions].each_with_index do |step, index|
+	puts "#{index +1}, #{step}"
 end
 
 
