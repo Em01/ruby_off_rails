7d9c29802c6b8925a6387ac7d97872a92c295756
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 14:42:00 +0000

    hash of ingredients and method now outputs

--- recipe_hash/recipe.rb
+++ recipe_hash/recipe.rb
@@ -1,1 +1,18 @@
-recipe.rb
+ingredients = {}
+ingredients[:avocados] = 4
+ingredients[:jalapenos] = 2
+
+
+recipe = {} #same as Hash.new
+recipe[:ingredients] = ingredients
+recipe[:method] = ["Peel / Slice Avocados", "Chop jalapenos into small dice"]
+
+puts "ingredients"
+recipe[:ingredients].each do |key, value|
+	puts "#{key}: #{value}"
+end
+
+puts "\nMethod"
+recipe[:method].each do |step|
+	puts step
+end
