a41164c1b6796a71494c15d1db637538e539f07c
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 15:25:18 +0000

    fixed errors and updated everything for struct

--- recipe_hash/recipe.rb
+++ recipe_hash/recipe.rb
@@ -2,13 +2,13 @@ ingredients = {}
 ingredients[:avocados] = 4
 ingredients[:jalapenos] = 2
 
-Recipe = Struct.new[:ingredients, :method]
+Recipe = Struct.new(:ingredients, :method)
 #recipe as a struct that has ingredients and a method
 
 
-recipe = {} #same as Hash.new
-recipe[:ingredients] = ingredients
-recipe[:method] = ["Peel / Slice Avocados", "Chop jalapenos into small dice"]
+recipe = Recipe.new
+recipe.ingredients = ingredients
+recipe.method = ["Peel / Slice Avocados", "Chop jalapenos into small dice"]
 
 puts "ingredients"
 recipe[:ingredients].each do |key, value|
