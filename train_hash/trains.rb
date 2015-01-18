859e2da8204746ae277c26e5b7e3bf85f121583f
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 16:25:01 +0000

    added passenger and person struct

--- train_hash/trains.rb
+++ train_hash/trains.rb
@@ -4,13 +4,28 @@ train[:engines] = 2
 train[:cars] = 1
 train[:caboose] = 1
 
+passenger = {}
+passenger[:name] = "Emma"
+
+
+
 Station = Struct.new(:train, :instructions)
 station = Station.new
 station.train = train
 station.instructions = ["Get on train", "Drive the train"]
 
+Person = Struct.new(:passenger) 
+person = Person.new
+person.passenger = passenger
+
+
 puts "train"
 station[:train].each do |key, value|
+	puts "* #{key}: #{value}"
+end
+
+puts "passenger"
+person[:passenger].each  do |key, value|
 	puts "* #{key}: #{value}"
 end
 
@@ -18,6 +33,8 @@ puts "\nInstructions"
 station[:instructions].each_with_index do |step, index|
 	puts "#{index +1}, #{step}"
 end
+
+
 
 
 
