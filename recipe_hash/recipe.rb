263f83181dc8cdfa4c6f722387380933701ad4ab
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 15:29:44 +0000

    refactoring

--- recipe_hash/recipe.rb
+++ recipe_hash/recipe.rb
@@ -5,10 +5,7 @@ ingredients[:jalapenos] = 2
 Recipe = Struct.new(:ingredients, :method)
 #recipe as a struct that has ingredients and a method
 
-
-recipe = Recipe.new
-recipe.ingredients = ingredients
-recipe.method = ["Peel / Slice Avocados", "Chop jalapenos into small dice"]
+recipe = Recipe.new( {avocados: 4, jalapenos: 2}, ["Peel / Slice Avocados", "Chop jalapenos into small dice"])
 
 puts "ingredients"
 recipe.ingredients.each do |key, value|
