97f25cd6fe6e658fdc115ac6c22613a466ab8ac7
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 19:22:08 +0000

    finished example of mocks and rspec

--- ferry_rspec/spec/ferry_spec.rb
+++ ferry_rspec/spec/ferry_spec.rb
@@ -1,1 +1,12 @@
-ferry_spec.rb
+require 'rspec'
+require 'ferry'
+describe Conductor do 
+
+	let(:engineer) { mock }
+	let(:conductor) { Conductor.new(engineer)}
+
+	it "should tell the enginner to slow down" do 
+		engineer.should_recieve(:slow_down!)
+		conductor.see_danger_coming!
+	end
+end
